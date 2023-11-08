5406ffb7b745b04b1920123803c77c4747d25597
erihidayat06 <erihidayat549@gmail.com>
2023-11-08 20:59:48 +0700

    Memperbaiki MainScreen.dart

--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:another_flushbar/flushbar.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -8,11 +10,64 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  TextEditingController isiController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(' page MainScreen'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.only(left: 10, right: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 10),
+            //tempat row
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "MOTIVASI",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.red),
+                  ),
+                  Text(
+                    "Visi Generasi Indonesia",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.red,
+                        fontStyle: FontStyle.italic),
+                  ),
+                ],
+              ),
+            ]),
+            // Akhir
+            SizedBox(height: 20),
+            FormBuilderTextField(
+              controller: isiController,
+              name: "isi_motivasi",
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.only(left: 10),
+              ),
+            ),
+
+            SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                  onPressed: () async {
+                    print("Sukses");
+                  },
+                  child: Text("Submit")),
+            ),
+          ],
+        ),
       ),
     );
   }
